9895a98e35b0452d8586bf0a15c547a76750f0c5
Giovani Aguirre <me@its-g.io>
2020-02-14 09:52:34 -0600

    Installed react-router and created components. Connected components to approprate parent components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './components/Dashboard';
+import Header from './components/Header';
+import Wizard from './components/Wizard';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Dashboard />
+      <Wizard />
+    </div>
+  );
+}
+
+export default App;
