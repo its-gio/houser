7f10f987b2d6498b6a2fe80764d6608630d618bf
Giovani Aguirre <me@its-g.io>
2020-02-14 11:11:40 -0600

    Created a map on dashboard to show all houses. (Non yet)

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,12 +4,20 @@ import House from './House'
 
 
 export default class Dashboard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      houses: []
+    }
+  }
   render() {
+    const houseMap = this.state.houses.map((house, i) => <House key={i} house={house} />)
     return (
       <div>
         <h2>Dashboard</h2>
         <Link to="/wizard/address"><button>Add New Property</button></Link>
-        <House />
+        { houseMap }
       </div>
     )
   }
